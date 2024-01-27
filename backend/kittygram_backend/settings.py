ed174053d4b413e0bae26b9fae8f12d8a92f9c31
Maxim <paranin.maxim@mail.ru>
2024-01-27 14:19:28 +0300

    Правки1

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,9 +9,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = '84.201.164.177', '127.0.0.1', 'localhost', 'kittyforyoualltime.serveblog.net'
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
